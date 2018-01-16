74f72355587a4b4a1dc42d7d7ab8aacecc2efd95
IvanTadeoHuerta <ivantec5sem@gmail.com>
2018-01-15 23:44:51 -0600

    Implementacion de materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
+
 
 @NgModule({
   declarations: [
