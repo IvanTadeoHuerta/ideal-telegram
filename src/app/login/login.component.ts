691f266bf62d540ec09c109d0979687dde9bd78f
IvanTadeoHuerta <ivantec5sem@gmail.com>
2018-02-19 23:43:02 -0600

    Login template html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: []
+  styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    }
+  );
+      $('.parallax').parallax();
   }
 
 }
